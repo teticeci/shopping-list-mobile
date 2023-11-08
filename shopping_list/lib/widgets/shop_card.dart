b25cc6b778b9e41259a29a02b6de073657b599ea
teticeci <chelseangelika+helo@gmail.com>
2023-11-08 14:31:39 +0700

    tutorial 7 selesai

--- shopping_list/lib/widgets/shop_card.dart
+++ shopping_list/lib/widgets/shop_card.dart
@@ -30,6 +30,12 @@ class ShopCard extends StatelessWidget {
           // Navigate ke route yang sesuai (tergantung jenis tombol)
           if (item.name == "Tambah Produk") {
             // TODO: Gunakan Navigator.push untuk melakukan navigasi ke MaterialPageRoute yang mencakup ShopFormPage.
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ShopFormPage(),
+              ),
+            );
           }
         },
         child: Container(
